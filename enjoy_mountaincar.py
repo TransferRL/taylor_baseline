f17c9ec4817176f8c2c70f66e7472e85db72fb26
Jeremy Ma <junweima2@gmail.com>
2017-11-22 12:36:20 -0500

    new model

--- enjoy_mountaincar.py
+++ enjoy_mountaincar.py
@@ -1,9 +1,8 @@
 import gym
 
-from baselines import deepq
+import deepq
 
 from lib.env.threedmountain_car import ThreeDMountainCarEnv
-
 
 def main():
     # env = gym.make("MountainCar-v0")
@@ -17,6 +16,7 @@ def main():
             # env.render()
             env.render_orthographic()
             obs, rew, done, _ = env.step(act(obs[None])[0])
+            print(act(obs[None])[0])
             print(obs)
             episode_rew += rew
         print("Episode reward", episode_rew)
--- train_mountaincar.py
+++ train_mountaincar.py
@@ -13,7 +13,7 @@ def main():
         env,
         q_func=model,
         lr=1e-3,
-        max_timesteps=100000,
+        max_timesteps=200000,
         buffer_size=50000,
         exploration_fraction=0.1,
         exploration_final_eps=0.1,
